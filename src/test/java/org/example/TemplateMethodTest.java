313277f0a829db62f4003f30baff75915864b4d1
jair-ferreira-rodrigues-neto <jairferreiraneto1704mg@gmail.com>
2026-05-11 20:56:47 -0300

    Implementando a lógica do padrão Template Method em um sistema de Exportação de Relatórios, adicionando testes e diagrama UML

---
+++ src/test/java/org/example/TemplateMethodTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TemplateMethodTest {
+
+    @Test
+    public void testExportacaoHTML() {
+        ExportadorRelatorio exportador = new ExportadorHTML();
+        String resultadoEsperado = "Arquivo salvo com o conteudo:\n" +
+                "<ul>\n" +
+                "  <li>Nome: Joao</li>\n" +
+                "  <li>Idade: 30</li>\n" +
+                "  <li>Cargo: Desenvolvedor</li>\n" +
+                "</ul>";
+
+        assertEquals(resultadoEsperado, exportador.exportar());
+    }
+
+    @Test
+    public void testExportacaoCSV() {
+        ExportadorRelatorio exportador = new ExportadorCSV();
+        String resultadoEsperado = "Arquivo salvo com o conteudo:\n" +
+                "Nome: Joao;Idade: 30;Cargo: Desenvolvedor";
+
+        assertEquals(resultadoEsperado, exportador.exportar());
+    }
+}
